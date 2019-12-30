4d6b4655f066dfd920638f487c6edccb19158ec3
Henry Pham <phamtiensy8696@gmail.com>
2019-12-31 00:01:49 +0700

    angular by Henry

--- KPI.Web/App_Start/BundleConfig.cs
+++ KPI.Web/App_Start/BundleConfig.cs
@@ -25,6 +25,12 @@ namespace KPI.Web
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                     "~/Scripts/libs/runtime*",
+                     "~/Scripts/libs/polyfills*",
+                     "~/Scripts/libs/vendor*",
+                     "~/Scripts/libs/main*"));
         }
     }
 }
